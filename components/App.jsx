3459d81b052acbd8d41f2891c3ac0431858eb577
Chacta Brice <chactaib@gmail.com>
2022-05-07 15:38:06 -0700

    created components folder with App.jsx, Footer.jsx, Navbar.jsx, and added render to index.js. Added styling to these components

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from './Navbar'
+import Footer from './Footer'
+
+export default function App() {
+  return (
+    <div>
+        <NavigationBar />
+
+        <Footer />
+    </div>
+  )
+}
+
---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, NavDropdownProps, Container, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
+
+export default class NavigationBar extends Component {
+  render() {
+    return (
+        <Navbar bg='dark' variant='dark' collapseOnSelect expand='lg'>
+        <Container fluid>
+        <Navbar.Brand href="App">ShowKaze</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav className="me-auto">
+          <Nav.Link href="#Link1">Link 1</Nav.Link>
+          <Nav.Link href="#Link2">Link 2</Nav.Link>
+          <Nav.Link href="#Link3">Link 3</Nav.Link>			
+        </Nav>
+        <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-primary">Search</Button>
+      </Form>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+}
