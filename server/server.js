d05abb7a53e4ddfcfbb1b65993df274077475366
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-05-07 16:59:41 -0700

    updated global error handler error format, add route for getting concert by state

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path');
-
+const controller = require('./controllers/testController.js')
 
 const app = express()
 
@@ -9,15 +9,30 @@ const PORT = 3000;
 
 app.use('/client', express.static((path.join(__dirname, '../client'))));
 
+
+// rounte test
+app.get('/type=concert/:venue.state', controller.getByState, (req, res) => {
+  res.status(200).json(res.locals.byState);
+})
+
+
+// all rountes error handler
 app.use((req, res) => {
   res.status(404).send("Asset not found :-(");
 })
 
+// global error handler
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send(err);
+  const defarltErr = {
+    log: 'error occurred in unknown middleware',
+    status: 400,
+    message: 'An error occurred'
+  }
+  const errorObj = Object.assign({}, defarltErr, err);
+  return res.status(errorObj.status).send(errorObj.message);
 })
 
+// start server
 app.listen(PORT, () => {
   console.log('Listening on port 3000...');
 })
