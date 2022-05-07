5993820a2fbeac5bc70b6ce7dac6464351a628ea
SalSaluga <38927965+SalSaluga@users.noreply.github.com>
2022-05-07 13:21:31 -1000

    We have created the route for fetching api data for concerts in a certain state. A test controller was added to ensure that route parameters were sent properly

--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path');
 
+const testController = require('./controllers/testcontrollers.js');
 
 const app = express()
 
 const PORT = 3000;
 
 app.use('/client', express.static((path.join(__dirname, '../client'))));
+
+//set up a route for get requests 
+//endpoint would be the location input by user
+  //if a certain location is called
+app.get('/location/:state', testController.testRouteParams, (req, res) => {
+  res.status(200).sendFile((path.join(__dirname, 'sal.html')));
+})
+
 
 app.use((req, res) => {
   res.status(404).send("Asset not found :-(");
@@ -21,3 +30,6 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log('Listening on port 3000...');
 })
+
+
+
