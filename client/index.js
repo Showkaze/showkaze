273ff888e09fb07195a3fc6abc3bce536fad3a39
Chacta Brice <chactaib@gmail.com>
2022-05-09 10:15:58 -0700

    Modified index.css, index.js, App.jsx, Navbar, Footer. Added fontawesome to package.json. Added Card_Item, Card_Section, Carousel.jsx

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import App from '../components/App'
 import './index.css'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 
 const container = document.getElementById('root')
