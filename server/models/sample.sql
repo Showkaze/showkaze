86204f02a9ce6b12e82a92176aecd20180e0137f
James Edwards <j.olden.edwards@gmail.com>
2022-05-08 20:53:52 -0700

    These files contain sample data for use when testing the functionality of the database and its associated middleware

---
+++ server/models/sample.sql
@@ -0,0 +1,8 @@
+INSERT INTO events(id, imageURL, artist, _date, city)
+VALUES
+  (1, 'img1', 'phish', 'nov 5', 'culver city'),
+  (2, 'img2', 'dmb', 'nov 6', 'humbolt'),
+  (3, 'img3', 'grateful dead', 'nov 7', 'san francisco'),
+  (4, 'img4', 'nickelback', 'nov 8', 'bevery hills'),
+  (5, 'img5', 'disturbed', 'nov 9', 'carmel')
+;
